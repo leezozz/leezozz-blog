976c440519fb421836c552711f7c39b51dad34d7
gaofuhong <157297759@qq.com>
2023-01-26 16:53:21 +0800

    fix: modify some config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,11 @@
 const { sideBarConfig }  = require('./src/sideBarData')
 const { sideBarFilter }  = require('./src/sideBarFilter')
+const { navList }  = require('./src/navList')
+
 // 设置根路径
 // const rootPath = '/Users/admin/Documents/work/vue3/leezozz-blog/js'
 
-console.log('66666', sideBarFilter(sideBarConfig))
+console.log('侧边栏数据：', sideBarFilter(sideBarConfig))
 
 
 
@@ -21,6 +23,7 @@ module.exports = {
         "href": "/favicon.ico"
       }
     ],
+    // 在移动端，搜索框在获得焦点时会放大，并且在失去焦点后可以左右滚动，这可以通过设置元来优化。
     [
       "meta",
       {
@@ -31,38 +34,7 @@ module.exports = {
   ],
   "theme": "reco",
   "themeConfig": {
-    "nav": [
-      {
-        "text": "首页",
-        "link": "/",
-        "icon": "reco-home"
-      },
-      {
-        "text": "基础",
-        "icon": "reco-blog",
-        "link": "/blog/base/",
-      },
-      {
-        "text": "工具",
-        "icon": "reco-other",
-        "link": "/blog/frameSeries/",
-      },
-      {
-        "text": "笔记",
-        "icon": "reco-suggestion",
-        "link": "/blog/view/",
-      },
-      {
-        "text": "时间轴",
-        "icon": "reco-date",
-        "link": "/timeline/",
-      },
-      {
-        "text": "GitHub",
-        "icon": "reco-message",
-        "link": "https://github.com/leezozz/leezozz-blog.git",
-      }
-    ],
+    "nav": navList,
     "sidebar": sideBarFilter(sideBarConfig),
     "type": "blog",
     "blogConfig": {
@@ -86,7 +58,6 @@ module.exports = {
         "link": "https://github.com/leezozz/leezozz-blog.git"
       }
     ],
-    // "logo": "/logo.png",
     "search": true,
     "searchMaxSuggestions": 10,
     "lastUpdated": "最近更新时间",
@@ -97,6 +68,49 @@ module.exports = {
     "startYear": "2022"
   },
   "markdown": {
+    // 是否显示代码块左侧行数
     "lineNumbers": true
-  }
+  },
+  plugins: [
+    [
+      "@vuepress-reco/vuepress-plugin-bgm-player",{
+        audios: [
+          // 本地文件示例
+          // {
+          //   name: '장가갈 수 있을까',
+          //   artist: '咖啡少年',
+          //   url: '/bgm/1.mp3',
+          //   cover: '/bgm/1.jpg'
+          // },
+          // 网络文件示例
+          {
+            name: '강남역 4번 출구',
+            artist: 'Plastic / Fallin` Dild',
+            url: 'https://assets.smallsunnyfox.com/music/2.mp3',
+            cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
+          },
+          {
+            name: '用胳膊当枕头',
+            artist: '최낙타',
+            url: 'https://assets.smallsunnyfox.com/music/3.mp3',
+            cover: 'https://assets.smallsunnyfox.com/music/3.jpg'
+          }
+        ]  
+      }
+    ],
+    [
+      '@vuepress-reco/vuepress-plugin-kan-ban-niang',{
+        theme: [
+          'blackCat', 'whiteCat', 'miku',  'haru1', 'haru2', 'haruto', 'koharu', 'izumi', 'shizuku', 'wanko',  'z16'
+        ],
+        clean: true,
+        // messages: { 
+        //   welcome: '欢迎来到我的博客', home: '心里的花，我想要带你回家。', theme: '好吧，希望你能喜欢我的其他小伙伴。', close: '你不喜欢我了吗？痴痴地望着你。' 
+        // },
+        messageStyle: { right: '60px', bottom: '290px' },
+        width: 130,
+        height: 200
+      }
+    ],
+  ]
 }
---
+++ docs/.vuepress/src/navList.js
@@ -0,0 +1,37 @@
+const navList = [
+  {
+    "text": "首页",
+    "link": "/",
+    "icon": "reco-home"
+  },
+  {
+    "text": "基础",
+    "icon": "reco-blog",
+    "link": "/blog/base/",
+  },
+  {
+    "text": "工具",
+    "icon": "reco-other",
+    "link": "/blog/frameSeries/",
+  },
+  {
+    "text": "笔记",
+    "icon": "reco-suggestion",
+    "link": "/blog/view/",
+  },
+  {
+    "text": "时间轴",
+    "icon": "reco-date",
+    "link": "/timeline/",
+  },
+  {
+    "text": "GitHub",
+    "icon": "reco-message",
+    "link": "https://github.com/leezozz/leezozz-blog.git",
+  }
+]
+
+
+module.exports = {
+  navList
+}
